66206b5507d2dbbbfb7ff1ccf17a46973c0caca9
ahungrynoob <dxd_sjtu@outlook.com>
2020-05-09 14:35:47 +0800

    test: add handle scroll test case

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 import React from 'react';
 import { render } from 'remax';
+import renderer from 'react-test-renderer';
 import AppContainer from 'remax/esm/AppContainer';
 
 const ENV = ['ali', 'wechat', 'toutiao'];
@@ -61,6 +62,59 @@ describe('src/index.tsx', () => {
         container,
       );
       expect(container.root.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  it('should render correct items when handle scroll', () => {
+    jest.isolateModules(() => {
+      const RecycleView = require('../src/index').default;
+      const { ScrollView } = require('../src/remax');
+      const mockData = [];
+      let testNumber = 0;
+      for (let i = 0; i < 500; i++) {
+        mockData.push({
+          height: 100,
+          text: i,
+        });
+      }
+      const component = renderer.create(
+        <RecycleView
+          className="recycle-view"
+          onScroll={() => {
+            testNumber++;
+          }}
+          data={mockData}
+          overscanCount={5}
+          headerHeight={100}
+          bottomHeight={100}
+          renderHeader={() => {
+            return <div style={{ height: 100, backgroundColor: 'yellow' }}>this is header</div>;
+          }}
+          renderBottom={() => {
+            return <div style={{ height: 100, backgroundColor: 'yellow' }}>this is bottom</div>;
+          }}
+          renderItem={({ text }) => {
+            return <div style={{ height: '100%' }}>{text}</div>;
+          }}
+        />,
+      );
+      const instance = component.root;
+      const recycleView = instance.findByType(ScrollView);
+
+      const itemCount = (component.toJSON().children[1] as any).children[0].children.length;
+      expect(itemCount).toBe(11);
+
+      // mock scroll to middle
+      renderer.act(() => {
+        const handleScroll = recycleView.props.onScroll;
+        handleScroll({ detail: { scrollTop: 25150, scrollHeight: 50200 } });
+      });
+
+      const items = (component.toJSON().children[1] as any).children[0].children;
+      const itemCountAfterScroll = items.length;
+      expect(itemCountAfterScroll).toBe(16);
+      expect(items[0].children[0].children[0]).toBe('245');
+      expect(testNumber).toBe(1);
     });
   });
 });
